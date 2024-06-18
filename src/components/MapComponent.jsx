a8a9e05d7a3a2ba5ce2d3d4bf69ec96d0c803a96
dadenkasandi08 <dadenkasandi43@mhs.unram.ac.id>
2024-06-18 23:25:31 +0800

    add peta

---
+++ src/components/MapComponent.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+let DefaultIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const MapComponent = () =>{
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} style ={{height: "100vh", width: "100%"}} >
+
+            
+        </MapContainer>
+    );
+}
--- src/pages/peta/index.tsx
+++ src/pages/peta/index.tsx
@@ -1,14 +1,15 @@
 import Layout from "@/layout/layout";
+import Map from "@/components/maps";
 
 function Peta() {
+  const position: [number, number] = [51.505, -0.09];
+
   return (
     <Layout>
       <div>
 
         <div className="py-24 px-32  text-center flex flex-col justify-center gap-6">
-          <p className="text-3xl font-bold text-secondary-800">
-            Tentang <span className="text-primary-600">Peta Bhineka</span>
-          </p>
+          <Map center={position} zoom={13}></Map>
           <div className="flex justify-start">
             <p className="text-2xl text-left max-w-[1050px]">
               <span className="text-3xl font-bold text-primary-600">Peta Bhineka</span>{" "}INI ISINYA PETA            </p>
